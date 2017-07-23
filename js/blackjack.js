127b63bbe6651ed30336b904b016a797cfb019db
David Shapiro <DavidS@dlsa.com>
2017-07-23 13:51:53 -0500

    xfer to CodePen

--- js/blackjack.js
+++ js/blackjack.js
@@ -1,10 +1,30 @@
 
+var Card = function(num, suit) {
+  this.value = [num, suit];
+  this.name = suit;
+  console.log(this.value[0]);
+
+  this.getName = function() {
+    return this.name;
+  }
+
+  this.setValue = function(num,suit) {
+    this.value = [num, suit];
+  }
+
+  this.getValue = function() {
+    return this.value;
+  }
+};
+
 function deal() {
  document.getElementById("msg1").innerHTML = "Dealing";  document.getElementById("msg2").innerHTML = "...";
 }
 
 function hit() {
   document.getElementById("msg1").innerHTML = "Hit me!";
+  var card = new Card(1, "Hearts");
+  document.getElementById("msg2").innerHTML = card.getValue()[1];
 }
 
 function stand() {
