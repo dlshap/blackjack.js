b78b7981f86b859d7bca7bd6fefebb728d48ff10
David Shapiro <DavidS@dlsa.com>
2017-07-25 10:45:29 -0500

    Added config

---
+++ js/config.js
@@ -0,0 +1,9 @@
+var config = {
+  cardFileURL: "http://www.dlsa.com/blackjack/",
+  cardPath: "images/cardicons/",
+  cardBackPath: "card-backs/b1fv_big.png",
+  cardFaceDir: "SVG-cards-1.3/",
+  cardFaceExt: ".svg",
+  noOfDecks: 1,
+  onlyDeal: "pairs"
+};
