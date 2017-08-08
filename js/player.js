7637d39f1c9111d619bedef92628d76e23470e4e
David Shapiro <DavidS@dlsa.com>
2017-08-08 17:59:25 -0500

    Makes more sense...took this out of hands in player proto

--- js/player.js
+++ js/player.js
@@ -2,19 +2,19 @@ function Player() {
   var hands = [];
   var playerName = "";
   this.handCount = function () {
-    return this.hands.length;
+    return hands.length;
   }
   this.addHand = function (hand) {
-    this.hands.push(hand);
+    hands.push(hand);
   }
   this.getHand = function (whichHand) {
-    if (this.hands.length === 0)
-      return null
+    if (hands.length === 0)
+      return null;
     else
-      return this.hands[whichHand];
+      return hands[whichHand];
   }
   this.clearHands = function () {
-    this.hands = [];
+    hands = [];
   }
   this.setName = function (name) {
     this.playerName = name;
